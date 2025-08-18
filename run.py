d4dae83cf1a945af2c6a36e648b7f5531b9d5140
RyryBioumlaTech <bpaulstevenryan@gmail.com>
2025-08-18 01:06:18 +0100

    update the export function

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app,host='0.0.0.0', port=5000, ssl_context=('cert.pem', 'key.pem'), debug=True)
+    socketio.run(app,host='127.0.0.1', port=5000, debug=True)
